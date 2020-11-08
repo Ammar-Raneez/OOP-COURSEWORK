f87c1a7197a0fb85f9f5c4b7cd53849adf6f87df
Ammar <ammarraneez@gmail.com>
2020-11-08 15:24:51 +0530

    pojo classes, 1st submission request complete

---
+++ coursework-cli/src/PremierLeagueManager.java
@@ -0,0 +1,45 @@
+import java.awt.*;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class PremierLeagueManager implements LeagueManager {
+    private static List<SportsClub> sportsClubs = new ArrayList<>();
+    private static Scanner sc = new Scanner(System.in);
+
+    @Override
+    public void addClub() throws ClassNotFoundException, IllegalAccessException {
+
+    }
+
+    @Override
+    public SportsClub deleteClub() {
+
+    }
+
+    @Override
+    public void displaySelectedClub() {
+
+    }
+
+    @Override
+    public void addPlayedMatch() {
+
+    }
+
+    @Override
+    public void displayPointsTable() {
+
+    }
+
+    @Override
+    public void saveData() {
+
+    }
+
+    @Override
+    public void loadData() {
+
+    }
+}
