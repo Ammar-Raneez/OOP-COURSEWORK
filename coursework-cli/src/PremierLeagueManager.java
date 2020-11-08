06827e42245dd851c3a6eefa06e22b0b64390eaf
Ammar <ammarraneez@gmail.com>
2020-11-08 15:27:18 +0530

    add method to be able to add a club done

--- coursework-cli/src/PremierLeagueManager.java
+++ coursework-cli/src/PremierLeagueManager.java
@@ -10,7 +10,50 @@ public class PremierLeagueManager implements LeagueManager {
 
     @Override
     public void addClub() throws ClassNotFoundException, IllegalAccessException {
+        System.out.println("Enter Club's name");
+        String clubName = sc.nextLine().toLowerCase();
+        System.out.println("Enter club location");
+        String clubLocation = sc.nextLine().toLowerCase();
+        System.out.println("Enter club owner");
+        String clubOwner = sc.nextLine().toLowerCase();
+        System.out.println("Enter club sponsor");
+        String clubSponsor = sc.nextLine().toLowerCase();
 
+        Color colorTop;
+        Color colorShort;
+        Field fieldTop;
+        Field fieldBottom;
+
+        while (true) {
+            try {
+                System.out.println("Enter club kit top color");
+                String topColorInput = sc.nextLine();
+                fieldTop = Class.forName("java.awt.Color").getField(topColorInput.toLowerCase());
+                colorTop = (Color) fieldTop.get(null);
+                break;
+            } catch (NoSuchFieldException e) {
+                System.out.println("[ERROR] ==> Please choose one of the valid colors! [black, blue, cyan, gray, green, magenta, orange, " +
+                        "pink, red, white, yellow]");
+            }
+        }
+
+        while (true) {
+            try {
+                System.out.println("Enter club kit short color");
+                String shortColorInput = sc.nextLine();
+                fieldBottom = Class.forName("java.awt.Color").getField(shortColorInput.toLowerCase());
+                colorShort = (Color) fieldBottom.get(null);
+                break;
+            } catch (NoSuchFieldException e) {
+                System.out.println("[ERROR] ==> Please choose one of the valid colors! [black, blue, cyan, gray, green, magenta, orange, " +
+                        "pink, red, white, yellow]");
+            }
+        }
+
+        SportsClub footballClub = new FootballClub(clubName, clubLocation, clubOwner, new ClubKit(clubSponsor, colorTop, colorShort));
+        sportsClubs.add(footballClub);
+        System.out.println(footballClub);
+        System.out.println(sportsClubs.size());
     }
 
     @Override
