0f48c7b107351fd6085fb841e3a1a870d4044be8
Ammar <ammarraneez@gmail.com>
2020-11-26 16:16:43 +0530

    understanding how play works

--- coursework-gui-AngularPFW/play!/coursework-server/app/controllers/TestController.java
+++ coursework-gui-AngularPFW/play!/coursework-server/app/controllers/TestController.java
@@ -4,6 +4,9 @@ import play.mvc.*;
 
 public class TestController extends Controller {
     public Result test() {
-        return ok(views.html.test.render());
+        //ok - status code 200
+        //this return is of type Result
+        //renders the test html file inside the views folder passing two parameters
+        return ok(views.html.test.render("some parameter text", "second parameter"));
     }
 }
