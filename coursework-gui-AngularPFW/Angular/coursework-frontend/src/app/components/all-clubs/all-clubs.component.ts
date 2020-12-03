96d5a319c873b037752a84349d90be7030748726
Ammar <ammarraneez@gmail.com>
2020-12-03 21:49:04 +0530

    styling is alright for now

--- coursework-gui-AngularPFW/Angular/coursework-frontend/src/app/components/all-clubs/all-clubs.component.ts
+++ coursework-gui-AngularPFW/Angular/coursework-frontend/src/app/components/all-clubs/all-clubs.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FootballClub } from 'src/app/models/FootballClub';
+import { AllClubsService } from 'src/app/services/all-clubs/all-clubs.service';
 
 @Component({
   selector: 'app-all-clubs',
@@ -6,10 +8,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./all-clubs.component.css']
 })
 export class AllClubsComponent implements OnInit {
+  allClubs : FootballClub[];
 
-  constructor() { }
+  constructor(private allClubsService : AllClubsService) { }
 
   ngOnInit(): void {
+    this.getFootballClubs();
   }
 
+  getFootballClubs() : void {
+    this.allClubsService.getAllFootballClubs().subscribe(
+      response => this.handleSuccessfulResponse(response),
+      error => this.handleErrorResponse(error)
+    );
+  }
+
+  handleSuccessfulResponse(response : any) : void {
+    this.allClubs = response;
+  }
+  handleErrorResponse(error : any) : void {
+    this.allClubs = error.message;
+    console.log(error);
+  }
 }
