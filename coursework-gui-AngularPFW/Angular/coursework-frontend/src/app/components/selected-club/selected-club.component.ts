4fed508ca180e15c5235495e0a069895b1ba834f
Ammar <ammarraneez@gmail.com>
2020-12-04 23:09:39 +0530

    star rating

--- coursework-gui-AngularPFW/Angular/coursework-frontend/src/app/components/selected-club/selected-club.component.ts
+++ coursework-gui-AngularPFW/Angular/coursework-frontend/src/app/components/selected-club/selected-club.component.ts
@@ -11,6 +11,8 @@ import { SelectedClubService } from 'src/app/services/selected-club/selected-clu
 export class SelectedClubComponent implements OnInit {
   selectedClub : FootballClub;
   clubName : string;
+  positionBackgroundColor: string[] = [];
+  starRatingCount: number[] = [];
 
   constructor(private selectedClubService : SelectedClubService, private route : ActivatedRoute) { }
 
@@ -27,10 +29,48 @@ export class SelectedClubComponent implements OnInit {
   }
   handleSuccessfulResponse(response : any) : void {
     this.selectedClub = response;
+    this.determinePositionBackgroundColor();
+    this.determineStarRatingCount();
     console.log(response);
   }
   handleErrorResponse(error : any) : void {
     this.selectedClub = error.message;
     console.log(error);
+  }
+
+  determinePositionBackgroundColor() : void {
+    const goldenRod = [ 'SW', 'LM', 'RM', 'CM', 'DM', 'AM' ];
+    const red = ['CF', 'SS', 'S', 'WF'];
+    const green = ['LB', 'RB', 'CB', 'LWB', 'RWB'];
+    // const purple = ['GK']
+
+    if (this.selectedClub) {
+      for(let player of this.selectedClub.allPlayers) {
+        console.log(player.position)
+        if(goldenRod.includes(player.position)) {
+          this.positionBackgroundColor.push('goldenrod');
+        } else if(red.includes(player.position)) {
+          this.positionBackgroundColor.push('red');
+        } else if(green.includes(player.position)) {
+          this.positionBackgroundColor.push('green');
+        } else {
+          this.positionBackgroundColor.push('purple');
+        }
+      }
+    }
+  }
+
+  determineStarRatingCount() : void {
+    if(this.selectedClub) {
+      for(let player of this.selectedClub.allPlayers) {
+        if(player.playerStats.overall < 50) {
+          this.starRatingCount.push(3);
+        } else if(player.playerStats.overall < 75) {
+          this.starRatingCount.push(4);
+        } else {
+          this.starRatingCount.push(5);
+        }
+      }
+    }
   }
 }
