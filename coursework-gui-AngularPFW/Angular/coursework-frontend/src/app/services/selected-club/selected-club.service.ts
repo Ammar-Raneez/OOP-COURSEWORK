50736b17fabca0dcbb35cf30c2a61340c1b75282
Ammar <ammarraneez@gmail.com>
2020-12-04 17:47:38 +0530

    selected club service done

--- coursework-gui-AngularPFW/Angular/coursework-frontend/src/app/services/selected-club/selected-club.service.ts
+++ coursework-gui-AngularPFW/Angular/coursework-frontend/src/app/services/selected-club/selected-club.service.ts
@@ -1,9 +1,14 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { FootballClub } from 'src/app/models/FootballClub';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SelectedClubService {
-
-  constructor() { }
+  constructor(private httpClient : HttpClient) {  }
+  
+  getSelectedClub(clubName : string) {
+    return this.httpClient.get<FootballClub>(`http://localhost:9000/allmatches/${clubName}`);
+  }
 }
