df06824e808815260b434b3cd29947ef0660c527
Ammar <ammarraneez@gmail.com>
2020-12-04 17:55:51 +0530

    backend api routes and controller method added to return a selected club

--- coursework-gui-AngularPFW/backend/app/controllers/ClubController.java
+++ coursework-gui-AngularPFW/backend/app/controllers/ClubController.java
@@ -5,11 +5,18 @@ import play.libs.Json;
 import java.util.*;
 
 public class ClubController extends Controller {
+    coursework.PremierLeagueManager leagueManager = new coursework.PremierLeagueManager();
+
     public Result returnAllClubs() {
         coursework.ConsoleApplication.loadData();
         List<coursework.FootballClub> allClubs = coursework.PremierLeagueManager.getAllFootballClubs();
         allClubs.sort(Collections.reverseOrder());
         return ok(Json.toJson(allClubs));
+    }
+
+    public Result returnSelectedClub(String clubName) {
+        coursework.FootballClub selectedClub = leagueManager.displaySelectedClub(clubName);
+        return ok(Json.toJson(selectedClub));
     }
 
     public Result winSortClubFilter() {
