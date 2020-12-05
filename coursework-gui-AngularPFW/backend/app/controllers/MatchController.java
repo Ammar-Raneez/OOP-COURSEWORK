aac1dc8614e1c66b63f960cf759cffa0f23ded1a
Ammar <ammarraneez@gmail.com>
2020-12-05 21:48:01 +0530

    Revert "connected console to gui, but in a stupid method lmao"
    This reverts commit fad3f1f7

---
+++ coursework-gui-AngularPFW/backend/app/controllers/MatchController.java
@@ -0,0 +1,52 @@
+package controllers;
+
+import play.mvc.*;
+import play.libs.Json;
+import java.util.*;
+
+public class MatchController extends Controller {
+    public Result returnAllMatches() {
+//        coursework.ConsoleApplication.loadData();
+        List<coursework.FootballMatch> allMatches = coursework.PremierLeagueManager.getAllMatches();
+        Collections.sort(allMatches);
+        return ok(Json.toJson(allMatches));
+    }
+
+    public Result returnSelectedMatch(Integer arrIndex) {
+//        coursework.ConsoleApplication.loadData();
+        coursework.FootballMatch selectedMatch = coursework.PremierLeagueManager.getAllMatches().get(arrIndex);
+        return ok(Json.toJson(selectedMatch));
+    }
+
+    public Result returnMatchesOnDate(String obtainedDate) {
+//        coursework.ConsoleApplication.loadData();
+        List<coursework.FootballMatch> allMatches = coursework.PremierLeagueManager.getAllMatches();
+        List<coursework.FootballMatch> filteredMatches = new ArrayList<>();
+
+        for (coursework.FootballMatch footballMatch : allMatches) {
+            if (String.valueOf(footballMatch.getMatchDate()).equals(obtainedDate)) {
+                filteredMatches.add(footballMatch);
+            }
+        }
+
+        return ok(Json.toJson(filteredMatches));
+    }
+
+    public Result playMatch() {
+//        coursework.ConsoleApplication.loadData();
+
+        try {
+            coursework.ConsoleApplication.addPlayedMatch();
+        } catch (Exception ignored) {}
+
+
+        List<coursework.FootballClub> updatedClubs = coursework.PremierLeagueManager.getAllFootballClubs();
+        updatedClubs.sort(Collections.reverseOrder());
+        return ok(Json.toJson(updatedClubs));
+//        List<coursework.FootballMatch> updatedMatches = coursework.PremierLeagueManager.getAllMatches();
+//        Collections.sort(updatedMatches);
+
+//        List<Object> allData = new ArrayList<>(Arrays.asList(updatedClubs, updatedMatches));
+//        return ok(Json.toJson(allData));
+    }
+}
